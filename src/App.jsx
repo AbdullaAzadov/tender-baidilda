a23854dedf11a034dbf85ea2dd5d622e73473975
AbdullaAzadov <123924334+AbdullaAzadov@users.noreply.github.com>
2024-10-08 16:11:26 +0500

    implement: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./pages/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <p style={{ fontSize: "120px" }}>
+                orem ipsum, dolor sit amet consectetur adipisicing elit.
+                Officiis facere quibusdam, unde a repellendus eum! Ratione,
+                exercitationem. Voluptatibus blanditiis labore saepe nulla velit
+                molestiae illum natus, asperiores nostrum, sunt nobis minus?
+                Assumenda, culpa delectus animi molestiae facilis sit unde iste
+                fuga sed quam, nihil cupiditate? Exercitationem quo pariatur
+                quos eos aliquam accusamus nobis molestiae sed? Mollitia quis,
+                dignissimos sit aperiam molestias rerum minima facilis!
+                Inventore recusandae pariatur, nihil accusantium in facilis est
+                molestiae quaerat maiores illo mollitia voluptatem asperiores,
+                exercitationem error aut odit adipisci impedit! Doloremque
+                deleniti laboriosam ullam? Aspernatur libero porro eum fuga,
+                inventore magnam eius? Aspernatur vel repellat vitae soluta
+                harum architecto consequatur, id autem, labore consectetur
+                repudiandae, impedit temporibus nemo commodi dolores itaque
+                earum aliquid ab nam!
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/pages/Header/Header.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import style from "./Header.module.css";
+
+export default function Header() {
+    const [showHeaderBg, setShowHeaderBg] = useState(false);
+
+    useEffect(() => {
+        function scroll() {
+            setShowHeaderBg(window.scrollY > 80);
+        }
+
+        document.addEventListener("scroll", scroll);
+        return () => document.removeEventListener("scroll", scroll);
+    }, []);
+
+    return (
+        <header
+            className={`${style.header} ${showHeaderBg ? "header-bg" : ""}`}
+        >
+            <div className="container">
+                <div className={style.logoWrapper}>
+                    <img src="images/logo.png" alt="Tender First" />
+                </div>
+                <nav>
+                    <a href="#home">Главная</a>
+                    <a href="#about">О нас</a>
+                    <a href="#services">Наши преимущества</a>
+                    <a href="#portfolio">Скрины</a>
+                    <a href="#pricing">Цены</a>
+                    <a href="#contact">Контакты</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
